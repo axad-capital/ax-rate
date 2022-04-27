b76e3d16688c7e2078aafc1b8fb4016624edcc6a
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-27 19:12:53 -0400

    added pixels

--- client/src/components/Thanks/ThanksLong.js
+++ client/src/components/Thanks/ThanksLong.js
@@ -6,6 +6,7 @@ const ThanksLong = () => {
         <div>
             <h1 style={{ textAlign: "center" }}>Check Out Your Auto Insurance Quotes</h1>
             <br />
+            <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=365&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
         </div>
     )
 }
