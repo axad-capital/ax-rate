1b9d514ffc8b01000bf88c55c127d0e387a4b049
Anthony Parrino <anthonyparrino90@gmail.com>
2022-06-29 13:43:55 -0400

    added thank you pixels

--- client/src/components/Thanks/ThanksFb.js
+++ client/src/components/Thanks/ThanksFb.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const ThanksFb = () => {
-  return (
-    <div>ThanksFb</div>
-  )
+    return (
+        <div>
+            <div style={{ textAlign: "center", margin: "50px" }}>
+                <h1>Thank you!</h1>
+                <h3>Your Form Has Been Submitted Successfully</h3>
+            </div>
+            <br />
+        </div>
+    )
 }
 
 export default ThanksFb
--- client/src/components/Thanks/ThanksGoog.js
+++ client/src/components/Thanks/ThanksGoog.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const ThanksGoog = () => {
-  return (
-    <div>ThanksGoog</div>
-  )
+    return (
+        <div>
+            <div style={{ textAlign: "center", margin: "50px" }}>
+                <h1>Thank you!</h1>
+                <h3>Your Form Has Been Submitted Successfully</h3>
+            </div>
+            <br />
+        </div>
+    )
 }
 
 export default ThanksGoog
